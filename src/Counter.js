edd2ca35031465d01bb3241e27d1be7872b848cb
sujangJin <sujang1@naver.com>
2021-05-16 12:25:52 +0900

    210516

---
+++ src/Counter.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react';
+
+class Counter extends Component {
+    state = {
+        number: 0,
+        fixedNumber: 0
+    }
+    
+    render() {
+        const { number, fixedNumber } = this.state;
+        return(
+            <div>
+                <h1>{number}</h1>
+                <h2>바뀌지 않는 값: {fixedNumber}</h2>
+                <button
+                    // onClick을 통해 버튼이 클릭되었을 대 호출할 함수를 지정합니다.
+                    onClick = { () => {
+                        this.setState(
+                            {
+                                number: number + 1
+                            },
+                            () => {
+                                console.log('방금 setState가 호출되었습니다.');
+                                console.log(this.state);
+                            }
+                        );
+
+                        // 위 코드와 아래 코드는 완전히 똑같은 기능을 합니다.
+                        // 아래 코드는 함수에서 바로 객체를 반환한다는 의미입니다.
+                        this.setState(prevState => ({
+                            number: prevState.number + 1
+                        }));
+                    }}
+                >
+                    +1
+                </button>
+            </div>
+        );
+    }
+}
+
+export default Counter;
