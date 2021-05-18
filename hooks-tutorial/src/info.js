29636a64f332a83ac3fcd0b849ffff6899442373
Su Jang, Jin <sujang1@naver.com>
2021-05-18 23:55:02 +0900

    210518

--- hooks-tutorial/src/info.js
+++ hooks-tutorial/src/info.js
@@ -1,22 +1,26 @@
 import React, { useReducer } from 'react';
+import useInputs from './useInputs';
 
+/*
 function reducer(state, action) {
     return {
         ...state,
         [action.name]: action.value
     };
 }
+*/
 
 const Info = () => {
-    const[state, dispatch] = useReducer(reducer, {
+    const[state, dispatch] = useInputs({
         name: '',
         nickname: ''
     });
     const { name, nickname } = state;
+    /*
     const onChange = e => {
         dispatch(e.target);
     };
-
+    */
     return (
         <div>
             <div>
