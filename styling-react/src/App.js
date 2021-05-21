9d7d4daf4ae10c70ecdc0326b65085932811bb21
Su Jang, Jin <sujang1@naver.com>
2021-05-21 23:01:25 +0900

    210521

--- styling-react/src/App.js
+++ styling-react/src/App.js
@@ -1,12 +1,11 @@
 import React, { Component } from 'react';
-//import SassComponent from './SassComponent'
-import CSSModule from './CSSModule'
+import StyledComponent from './StyledComponent';
 
 class App extends Component {
   render() {
     return (
       <div>
-        <CSSModule />
+        <StyledComponent />
       </div>
     )
   }
