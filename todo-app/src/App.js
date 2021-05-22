88853f39993ee5548eba8886e29c9d2345fb1fe5
Su Jang, Jin <sujang1@naver.com>
2021-05-22 17:09:02 +0900

    210522

--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -1,7 +1,9 @@
-import logo from './logo.svg';
+import React from 'react';
+import TodoTemplate from './components/TodoTemplate';
+
 
 const App = () => {
-  return <div>Todo App을 만들자!</div>;
-}
+  return <TodoTemplate>Todo App을 만들자!</TodoTemplate>;
+};
 
 export default App;
