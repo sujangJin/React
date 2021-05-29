4275659fc3452b349dd9409db1e08c498313f5db
Su Jang, Jin <sujang1@naver.com>
2021-05-29 16:48:52 +0900

    210529

--- router-tutorial/src/App.js
+++ router-tutorial/src/App.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { Route, Link } from 'react-router-dom';
 import About from './About';
 import Home from './Home';
+import Profiles from './Profiles';
+import HistorySample from './HistorySample';
 
 const App = () => {
   return(
@@ -13,10 +15,18 @@ const App = () => {
         <li>
           <Link to="/about">소개</Link>
         </li>
+        <li>
+          <Link to="/profiles">프로필</Link>
+        </li>
+        <li>
+          <Link to="/history">History 예제</Link>
+        </li>
       </ul>
       <hr />
       <Route path="/" component={Home} exact={true}/>
-      <Route path="/about" component={About} />
+      <Route path={['/about', '/info']} component={About} />
+      <Route path="/profiles" component={Profiles} />
+      <Route path="/history" component={HistorySample} />
     </div>
   );
 };
