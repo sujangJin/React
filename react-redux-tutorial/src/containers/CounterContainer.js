9769f72b6e2c00540f1eccea6aef47343a6abfbc
Su Jang, Jin <sujang1@naver.com>
2021-06-12 17:09:37 +0900

    210612

--- react-redux-tutorial/src/containers/CounterContainer.js
+++ react-redux-tutorial/src/containers/CounterContainer.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
 import Counter from '../components/Counter';
 import { increase, decrease } from '../modules/counter';
@@ -9,21 +10,12 @@ const CounterContainer = ({ number, increase, decrease }) => {
     );
 };
 
-const mapStateToProps = state => ({
-    number: state.counter.number,
-});
-
-const mapDispatchToProps = dispatch => ({
-    // 임시 함수
-    increase: () => {
-        dispatch(increase());
-    },
-    decrease: () => {
-        dispatch(decrease());
-    },
-});
-
 export default connect(
-    mapStateToProps,
-    mapDispatchToProps,
+    state => ({
+        number: state.counter.number,
+    }),
+    {
+        increase,
+        decrease,
+    },
 )(CounterContainer);
--- react-redux-tutorial/src/index.js
+++ react-redux-tutorial/src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
-import { composeWithDevTools} from 'redux-devtools-extension';
+import { composeWithDevTools } from 'redux-devtools-extension';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
