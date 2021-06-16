d4342035924527c908d983ea865bf206202d2d3b
Su Jang, Jin <sujang1@naver.com>
2021-06-16 23:03:38 +0900

    210616

--- learn-redux-middleware/src/index.js
+++ learn-redux-middleware/src/index.js
@@ -9,9 +9,15 @@ import rootReducer from './modules';
 //import loggerMiddleware from './lib/loggerMiddleware';
 import { createLogger } from 'redux-logger';
 import ReduxThunk from 'redux-thunk';
+import createSagaMiddleware from 'redux-saga';
 
 const logger = createLogger();
-const store = createStore(rootReducer, applyMiddleware(logger, ReduxThunk));
+const sagaMiddleware = createSagaMiddleware();
+const store = createStore(
+  rootReducer, 
+  applyMiddleware(logger, ReduxThunk, sagaMiddleware)
+);
+sagaMiddleware.run(rootSaga);
 
 ReactDOM.render(
   <Provider store={store}>
