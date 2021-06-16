633c2adb6e32bb440862255f210770e892bddd3a
Su Jang, Jin <sujang1@naver.com>
2021-06-16 23:24:32 +0900

    210616

--- learn-redux-middleware/src/index.js
+++ learn-redux-middleware/src/index.js
@@ -5,17 +5,18 @@ import { Provider } from 'react-redux';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import rootReducer from './modules';
+import rootReducer, { rootSaga } from './modules';
 //import loggerMiddleware from './lib/loggerMiddleware';
 import { createLogger } from 'redux-logger';
 import ReduxThunk from 'redux-thunk';
 import createSagaMiddleware from 'redux-saga';
+import { composeWithDevTools } from 'redux-devtools-extension';
 
 const logger = createLogger();
 const sagaMiddleware = createSagaMiddleware();
 const store = createStore(
   rootReducer, 
-  applyMiddleware(logger, ReduxThunk, sagaMiddleware)
+  composeWithDevTools(applyMiddleware(logger, ReduxThunk, sagaMiddleware))
 );
 sagaMiddleware.run(rootSaga);
 
