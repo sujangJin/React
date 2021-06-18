ddba7f04077d998629af40b6e9ac1c712e737220
Su Jang, Jin <sujang1@naver.com>
2021-06-18 22:42:29 +0900

    210618

--- splitting-sample/src/App.js
+++ splitting-sample/src/App.js
@@ -1,20 +1,7 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import notify from './notify';
 
-function App() {
-  const onClick = () => {
-    notify();
-  };
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p onClick={onClick}>Hello React!</p>
-      </header>
-    </div>
-  );
+class App extends Component {
+  
 }
-
-export default App;
