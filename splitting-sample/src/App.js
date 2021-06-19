6ff29163e82dd788a5ef678fbaee4b040d964edd
Su Jang, Jin <sujang1@naver.com>
2021-06-19 23:06:12 +0900

    210619

--- splitting-sample/src/App.js
+++ splitting-sample/src/App.js
@@ -1,7 +1,24 @@
-import React, { Component } from 'react';
+import React, { useState, Suspense } from 'react';
 import logo from './logo.svg';
 import './App.css';
+const SplitMe = React.lazy(() => import('./SplitMe'));
 
-class App extends Component {
-  
+function App() {
+  const [visible, setVisible] = useState(false);
+  const onClick = () => {
+    setVisible(true);
+  };
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p onClick={onClick}>Hello React!</p>
+        <Suspense fallback={<div>loading...</div>}>
+          {visible && <SplitMe />}
+        </Suspense>
+      </header>
+    </div>
+  );
 }
+
+export default App;
