58bfdd594c7f34967a6d7d00e7bb1b28ae8255ad
Su Jang, Jin <sujang1@naver.com>
2021-06-20 23:31:19 +0900

    210620

--- ssr-recipe/config/paths.js
+++ ssr-recipe/config/paths.js
@@ -67,6 +67,8 @@ module.exports = {
   proxySetup: resolveApp('src/setupProxy.js'),
   appNodeModules: resolveApp('node_modules'),
   swSrc: resolveModule(resolveApp, 'src/service-worker'),
+  ssrIndexJs: resolveApp('src/index.server.js'), // 서버 사이드 렌더링 엔트리
+  ssrBuild: resolveApp('dist'),
   publicUrlOrPath,
 };
 
