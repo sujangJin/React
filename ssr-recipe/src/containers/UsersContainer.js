725a9f69a3a717ebb51d0fcbdb254a570dfc2617
Su Jang, Jin <sujang1@naver.com>
2021-07-03 23:40:51 +0900

    210703

---
+++ ssr-recipe/src/containers/UsersContainer.js
@@ -0,0 +1,22 @@
+import React, { useEffect } from 'react';
+import Users from '../components/Users';
+import { connect } from 'react-redux';
+import { getUsers } from '../modules/users';
+
+const UsersContainer = ({ users, getUsers}) => {
+    // 컴포넌트가 마운트되고 나서 호출
+    useEffect(() => {
+        if (users) return;
+        getUsers();
+    }, [getusers, users]);
+    return <Users users={users} />;
+};
+
+export default connect(
+    state => ({
+        users: state.users.users
+    }),
+    {
+        getUsers
+    }
+)(UsersContainer);
