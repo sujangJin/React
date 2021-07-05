c27ae8ccc0580d752a6c9c55eacf315ce721b497
Su Jang, Jin <sujang1@naver.com>
2021-07-05 23:17:40 +0900

    210705

--- ssr-recipe/src/containers/UsersContainer.js
+++ ssr-recipe/src/containers/UsersContainer.js
@@ -1,7 +1,10 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import Users from '../components/Users';
 import { connect } from 'react-redux';
 import { getUsers } from '../modules/users';
+import { Preloader } from '../lib/PreloadContext';
+
+const { useEffect } = React;
 
 const UsersContainer = ({ users, getUsers}) => {
     // 컴포넌트가 마운트되고 나서 호출
@@ -9,7 +12,12 @@ const UsersContainer = ({ users, getUsers}) => {
         if (users) return;
         getUsers();
     }, [getUsers, users]);
-    return <Users users={users} />;
+    return (
+        <>
+            <Users users={users} />
+            <Preloader resolve={getUsers} />
+        </>
+    );
 };
 
 export default connect(
--- ssr-recipe/src/index.server.js
+++ ssr-recipe/src/index.server.js
@@ -5,6 +5,12 @@ import { StaticRouter } from 'react-router-dom';
 import App from './App';
 import path from 'path';
 import fs from 'fs';
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
+import rootReducer from './modules';
+import PreloadContext from './lib/PreloadContext';
+import reactDom from 'react-dom';
 
 // asset-manifest.json에서 파일 경로들을 조회합니다.
 const manifest = JSON.parse(
@@ -16,7 +22,7 @@ const chunks = Object.keys(manifest.files)
     .map(key => `<script src="${manifest[key]}"></script>`) // 스크립트 태그로 변환하고
     .join(''); // 합침
 
-function createPage(root) {
+function createPage(root, stateScript) {
     return `<!DOCTYPE html>
     <html lang="en">
     <head>
@@ -35,6 +41,7 @@ function createPage(root) {
         <div id="root">
             ${root}
         </div>
+        ${stateScript}
         <script src="${manifest['runtime-main.js']}"></script>
         ${chunks}
         <script src="${manifest['main.js']}"></script>
@@ -50,13 +57,36 @@ const serverRender = (req, res, next) => {
     // 이 함수는 4040가 더야 하는 상황에 404를 띄우지 않고 서버 사이드 렌더링을 해 줍니다.
 
     const context = {};
+    const store = createStore(rootReducer, applyMiddleware(thunk));
+
+    const preloadContext = {
+        done : false,
+        promises: []
+    };
     const jsx = (
-        <StaticRouter location={req.url} context={context}>
-            <App />
-        </StaticRouter>
+        <PreloadContext.Provider value={preloadContext}>
+            <Provider store = {store}>
+                <StaticRouter location={req.url} context={context}>
+                    <App />
+                </StaticRouter>
+            </Provider>
+        </PreloadContext.Provider>    
     );
-    const root = ReactDOMServer.renderToString(jsx); // 렌더링을 하고
-    res.send(createPage(root)); // 결과물을 응답합니다.
+
+    ReactDOMServer.renderToStaticMarkup(jsx); // renderToStaticMarkup으로 한번 렌더링됩니다.
+    try {
+        await Promise.all(preloadContext.promises); // 모든 프로미스를 기다립니다.
+    } catch (e) {
+        return res.status(500);
+    }
+    preloadContext.done = true;
+    const root = ReactDOMServer.renderToString(jsx); // 렌더링을 합니다.
+    // JSON을 문자열로 변환하고 악성 스크립트가 실행되는 것을 방지하기 위해 <를 치환 처리
+    // https://redux.js.org/recipes/server-rendering#security-considerations
+    const stateString = JSON.stringify(store.getState()).replace(/</g, '\\u003c');
+    const stateScript = `<script>__PRELOADED_STATE__ = ${stateString}</script>`;
+    // 리덕스 초기 상태를 스크립트로 주입합니다.
+    res.send(createPage(root, stateScript)); // 결과물을 응답합니다.
 };
 
 const serve = express.static(path.resolve('./build'), {
