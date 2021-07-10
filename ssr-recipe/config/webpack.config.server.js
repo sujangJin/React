f0f88553b25586e9dab493465b5a31e75a1a7eb0
Su Jang, Jin <sujang1@naver.com>
2021-07-10 23:39:58 +0900

    210710

--- ssr-recipe/config/webpack.config.server.js
+++ ssr-recipe/config/webpack.config.server.js
@@ -1,15 +1,20 @@
 const paths = require('./paths');
 const getCSSModuleLocalIdent = require("react-dev-utils/getCSSModuleLocalIdent");
+const nodeExternals = require('webpack-node-externals');
+const webpack = require('webpack');
+const getClientEnvironment = require('./env');
 
 const cssRegex = /\.css$/;
 const cssModuleRegex = /\.module\.css$/;
 const sassRegex = /\.(scss|sass)$/;
 const sassModuleRegex = /\.module\.(scss|sass)$/;
 
+const env = getClientEnvironment(paths.publicUrlOrPath.slice(0, -1));
+
 module.exports = {
     mode: 'production', // 프로덕션 모드로 설정해 최적화 옵션들을 활성화
     entry: paths.ssrIndexJs, // 엔트리 경로
-    target: "node", // node 환경에서 실해오딜 것이라는 점을 명시
+    target: 'node', // node 환경에서 실해오딜 것이라는 점을 명시
     output: {
         path: paths.ssrBuild, // 빌드 경로
         filename: "server.js", // 파일 경로
@@ -63,10 +68,10 @@ module.exports = {
                         //  exportOnlyLocals: true 옵션을 설정해야 실제 css 파일을 생성하지 않습니다.
                         loader: require.resolve("css-loader"),
                         options: {
-                        importLoaders: 1,
-                        modules: {
-                            exportOnlyLocals: true,
-                        },
+                            importLoaders: 1,
+                            modules: {
+                                exportOnlyLocals: true,
+                            },
                         },
                     },
                     // CSS Module 을 위한 처리
@@ -74,11 +79,11 @@ module.exports = {
                         test: cssModuleRegex,
                         loader: require.resolve("css-loader"),
                         options: {
-                        importLoaders: 1,
-                        modules: {
-                            exportOnlyLocals: true,
-                            getLocalIdent: getCSSModuleLocalIdent,
-                        },
+                            importLoaders: 1,
+                            modules: {
+                                exportOnlyLocals: true,
+                                getLocalIdent: getCSSModuleLocalIdent,
+                            },
                         },
                     },
                     // Sass 를 위한 처리
@@ -89,52 +94,52 @@ module.exports = {
                         {
                             loader: require.resolve("css-loader"),
                             options: {
-                            importLoaders: 3,
-                            modules: {
-                                exportOnlyLocals: true,
-                            },
+                                importLoaders: 3,
+                                modules: {
+                                    exportOnlyLocals: true,
+                                },
                             },
                         },
                         require.resolve("sass-loader"),
-                        ],
-                    },
-                    // Sass + CSS Module 을 위한 처리
+                    ],
+                },
+                // Sass + CSS Module 을 위한 처리
+                {
+                    test: sassRegex,
+                    exclude: sassModuleRegex,
+                    use: [
                     {
-                        test: sassRegex,
-                        exclude: sassModuleRegex,
-                        use: [
-                        {
-                            loader: require.resolve("css-loader"),
-                            options: {
+                        loader: require.resolve("css-loader"),
+                        options: {
                             importLoaders: 3,
                             modules: {
                                 exportOnlyLocals: true,
                                 getLocalIdent: getCSSModuleLocalIdent,
                             },
-                            },
                         },
-                        require.resolve("sass-loader"),
-                        ],
                     },
-                    // url-loader 를 위한 설정
-                    {
-                        test: [/\.bmp$/, /\.gif$/, /\.jpe?g$/, /\.png$/],
-                        loader: require.resolve("url-loader"),
-                        options: {
+                    require.resolve("sass-loader"),
+                    ],
+                },
+                // url-loader 를 위한 설정
+                {
+                    test: [/\.bmp$/, /\.gif$/, /\.jpe?g$/, /\.png$/],
+                    loader: require.resolve("url-loader"),
+                    options: {
                         emitFile: false, // 파일을 따로 저장하지 않는 옵션
                         limit: 10000, // 원래는 9.76KB가 넘어가면 파일로 저장하는데
                         // emitFile 값이 false 일땐 경로만 준비하고 파일은 저장하지 않습니다.
                         name: "static/media/[name].[hash:8].[ext]",
-                        },
                     },
-                    // 위에서 설정된 확장자를 제외한 파일들은
-                    // file-loader 를 사용합니다.
-                    {
-                        loader: require.resolve("file-loader"),
-                        exclude: [/\.(js|mjs|jsx|ts|tsx)$/, /\.html$/, /\.json$/],
+                },
+                // 위에서 설정된 확장자를 제외한 파일들은
+                // file-loader 를 사용합니다.
+                {
+                    loader: require.resolve("file-loader"),
+                    exclude: [/\.(js|mjs|jsx|ts|tsx)$/, /\.html$/, /\.json$/],
                         options: {
-                        emitFile: false, // 파일을 따로 저장하지 않는 옵션
-                        name: "static/media/[name].[hash:8].[ext]",
+                            emitFile: false, // 파일을 따로 저장하지 않는 옵션
+                            name: "static/media/[name].[hash:8].[ext]",
                         },
                     },
                 ]
@@ -143,5 +148,13 @@ module.exports = {
     },
     resolve: {
         modules: ['node_modules']
-    }
+    },
+    externals: [
+        nodeExternals({
+            allowlist: [/@babel/],
+        })
+    ],
+    plugins: [
+        new webpack.DefinePlugin(env.stringified), // 환경변수를 주입해줍니다.
+    ]
 };
